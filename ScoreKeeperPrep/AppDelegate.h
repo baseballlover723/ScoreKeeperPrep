89f32d98dc2dabf017f63bf9c912417695fa0862
Philip Ross <rosspa@Philips-Mac.local>
2015-01-13 15:23:08 -0500

    Initial Commit

---
+++ ScoreKeeperPrep/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ScoreKeeperPrep
+//
+//  Created by Philip Ross on 1/13/15.
+//  Copyright (c) 2015 Philip Ross. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ScoreKeeperPrep/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ScoreKeeperPrep
+//
+//  Created by Philip Ross on 1/13/15.
+//  Copyright (c) 2015 Philip Ross. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
