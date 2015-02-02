928797562ab4791b79fe0c70d378f8a1669817fa
Philip Ross <rosspa@rose-hulman.edu>
2015-02-01 23:38:45 -0500

    initial commit

--- ScoreKeeperPrep/ViewController.m
+++ ScoreKeeperPrep/ViewController.m
@@ -9,6 +9,10 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *testTextField;
+@property (weak, nonatomic) IBOutlet UITextView *testTextView;
+
+@property (nonatomic) int player1Total;
 
 @end
 
@@ -17,11 +21,24 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.testTextView.text = @"";
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)pressedButton:(id)sender {
+    NSLog(@"Pressed Button");
+    NSString* textInTextField = self.testTextField.text;
+    
+    if (self.testTextView.text.length > 0) {
+    self.testTextView.text = [NSString stringWithFormat:@"%@\n%@", self.testTextView.text, textInTextField];
+    } else {
+        self.testTextView.text =textInTextField;
+    }
+    NSLog(textInTextField);
+    int textAsInteger = [textInTextField intValue];
+    self.player1Total += textAsInteger;
+    self.testTextView.text = [NSString stringWithFormat:@"%@\n%d", textInTextField, self.player1Total];
+    NSLog(self.testTextView.text);
+//    [self.view endEditing:YES];
 }
 
 @end
